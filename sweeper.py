a8fa7df563ef9698ccc2fde3a8bab6004a765b93
Jakub Grzelak <jakub.grzelak@weeve.network>
2021-05-20 12:16:53 +0000

    # Compliance * parameters passed in environment variables * docker-compose added

--- sweeper.py
+++ sweeper.py
@@ -7,64 +7,51 @@ from decouple import config
 log = logging.getLogger(__name__)
 app = Flask(__name__)
 __EGRESS_API_HOST__ = config('EGRESS_API_HOST')
-__EGRESS_API_PORT__ = config('EGRESS_API_PORT')
-__EGRESS_API_URI__ = config('EGRESS_API_URI')
 __EGRESS_API_METHOD__ = config('EGRESS_API_METHOD')
 __HANDLER_HOST__ = config('HANDLER_HOST')
 __HANDLER_PORT__ = config('HANDLER_PORT')
 
-# Set default settings
-settings = {
-    "upper_threshold": 1000,
-    "lower_threshold": -10,
-    "anomaly_positive_rate": 20,
-    "anomaly_negative_rate": 20,
-    "outliers": 'keep',  # keep, remove, smooth
-    "input_label": 'temperature',
-    "input_unit": 'Celsius',
-    "output_label": 'differentialTemp',
-    "output_unit": 'Celsius'
-}
-
-# Flag if settings are set
-settingsSet = False
+# Set module settings
+__UPPER_THRESHOLD__ = float(config('UPPER_THRESHOLD'))
+__LOWER_THRESHOLD__ = float(config('LOWER_THRESHOLD'))
+__ANOMALY_POSITIVE_RATE_OF_CHANGE__ = float(
+    config('ANOMALY_POSITIVE_RATE_OF_CHANGE'))
+__ANOMALY_NEGATIVE_RATE_OF_CHANGE__ = float(
+    config('ANOMALY_NEGATIVE_RATE_OF_CHANGE'))
+__OUT_OF_BOUND_DATA__ = config('OUT-OF-BOUND_DATA')
+__INPUT_LABEL__ = config('INPUT_LABEL')
+__INPUT_UNIT__ = config('INPUT_UNIT')
+__OUTPUT_LABEL__ = config('OUTPUT_LABEL')
+__OUTPUT_UNIT__ = config('OUTPUT_UNIT')
 
 # Previously received data value (for smoothing)
 prev_data = 0
 prev_time = time.time()
 first_data = True
 
+
 @app.route('/handle', methods=['POST'])
 def handle():
     '''
-    Receive ReST API POST request. First request is Sweeper settings.
-    The next requests are data.
+    Receive ReST API POST request with data.
     '''
-    global settings
-    global settingsSet
     global prev_time
 
-    if not settingsSet:
-        # set Sweeper settings
-        settings = request.get_json(force=True)
-        #print(f"Settings Request {settings}")
-        settingsSet = True
-        prev_time = time.time()
+    # receive data
+    received_data = request.get_json(force=True)
+    data_time = time.time()
 
-    else:
-        # receive data
-        received_data = request.get_json(force=True)
-        data_time = time.time()
+    # parse target data from the structure
+    try:
+        parsed_data = received_data[__INPUT_LABEL__]
+        #print("RECEIVED DATA: ", received_data)
+    except:
+        log.exception(f"Wrong data structure.")
 
-        # parse target data from the structure
-        try:
-            parsed_data = received_data[settings['input_label']]
-            #print("RECEIVED DATA: ", received_data)
-
-            analytics(parsed_data, data_time)
-        except:
-            log.exception(f"Wrong data structure.")
-
+    try:
+        analytics(parsed_data, data_time)
+    except:
+        log.exception(f"Data analysis failed.")
 
     # 204 - return success, no content status
     return '', 204
@@ -78,40 +65,41 @@ def analytics(data, data_time):
     rate_change = (data - prev_data) / (data_time - prev_time)
 
     # if the first data received
-    if first_data and settings['lower_threshold'] <= data <= settings['upper_threshold']:
+    if first_data and __LOWER_THRESHOLD__ <= data <= __UPPER_THRESHOLD__:
         first_data = False
         prev_data = data
         prev_time = data_time
         output(data)
-
-    if settings['lower_threshold'] <= data <= settings['upper_threshold'] and settings['anomaly_negative_rate'] <= rate_change <= settings['anomaly_positive_rate']:
+    elif __LOWER_THRESHOLD__ <= data <= __UPPER_THRESHOLD__ and __ANOMALY_NEGATIVE_RATE_OF_CHANGE__ <= rate_change <= __ANOMALY_POSITIVE_RATE_OF_CHANGE__:
         # not anomalous
         prev_data = data
         prev_time = data_time
         output(data)
     else:
         # anomalous
-        if settings['outliers'] == 'keep':
+        if __OUT_OF_BOUND_DATA__ == 'keep':
             prev_data = data
             prev_time = data_time
             output(data)
-        elif settings['outliers'] == 'smooth':
+        elif __OUT_OF_BOUND_DATA__ == 'smooth':
             prev_time = data_time
             output(prev_data)
-        
-        # elif settins['outliers'] == 'remove': DO NOTHING
+
+        # elif __OUT_OF_BOUND_DATA__ == 'remove': DO NOTHING
 
 
 def output(data):
     # prepare output HTTP ReST API request, build JSON body
     return_body = {
-        str(settings["output_label"]): data,
-        "input_unit": settings["output_unit"]
+        __OUTPUT_LABEL__: data,
+        "input_unit": __OUTPUT_UNIT__
     }
 
     # post request
     if __EGRESS_API_METHOD__ == "POST":
-        resp = requests.post(url=f"http://{__EGRESS_API_HOST__}:{__EGRESS_API_PORT__}{__EGRESS_API_URI__}", json=return_body)
+        resp = requests.post(
+            url=f"{__EGRESS_API_HOST__}", data=return_body)
+        #print(f"RETURN_BODY: {return_body}")
         #print(f"THE RESPONSE:{resp} {resp.text}")
     else:
         log.exception(f"The HTTP Method not supportive.")
